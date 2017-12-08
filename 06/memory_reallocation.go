0248f2e1b35b43048fad49ea0827c23d74aa30ad
Loraine <loraine@lorainekv.com>
2017-12-08 02:59:54 -0800

    Green test for blockReallocation

--- 06/memory_reallocation.go
+++ 06/memory_reallocation.go
@@ -28,10 +28,37 @@ func reallocationCycles(input string) int {
     hasUniqueState = logUniqueState(intSlice, log)
 
     // Find the largest bank
-    //largestBankIndex = findLargestBank(intSlice)
+    //largestBankIndex := findLargestBank(intSlice)
     // Zero it out, then redistribute the blocks
+    // reallocateBlocks(largestBankIndex)
   }
   return cycleCount
+}
+
+func reallocateBlocks(largestBankIndex int, intSlice []int) {
+  blocksToDistribute := intSlice[largestBankIndex]
+  intSlice[largestBankIndex] = 0
+  maxIndex := len(intSlice) - 1
+  var currentIndex int
+
+  // Set current index to the one after the largest bank OR first
+  if (largestBankIndex + 1) > maxIndex {
+    currentIndex = 0
+  } else {
+    currentIndex = largestBankIndex + 1
+  }
+
+  for blocksToDistribute > 0 {
+    intSlice[currentIndex] += 1
+    blocksToDistribute -= 1
+
+
+    if currentIndex == maxIndex {
+      currentIndex = 0
+    } else {
+      currentIndex += 1
+    }
+  }
 }
 
 func findLargestBank(intSlice []int) int {
